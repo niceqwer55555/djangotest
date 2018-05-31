d294be2c51e12306f4a452a96f3ffd4f8f98c8e5
root <root@localhost>
2018-05-31 17:52:59 +0800

    生成自定义的模板

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib import admin
+from cryptography.hazmat.primitives.ciphers import modes
 
 
 # Create your models here.
@@ -13,3 +14,36 @@ class BlogAdmin(admin.ModelAdmin):
     list_display = ('title','timestamp')
 
 admin.site.register(Blog, BlogAdmin)
+
+#在这里创造你自己的模板
+#作者
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField()
+    
+    def _unicode_(self):
+        return self.first_name
+    
+
+#出版商
+class Publisher(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=60)
+    state_province =models.CharField(max_length=30)
+    country = models.CharField(max_length=50)
+    website = models.URLField()
+    
+    def _unicode_(self):
+        return self.name
+    
+#书
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE,)
+    publication_date = models.DateField()
+    
+    def _unicode_(self):
+        return self.title
